0dd2c71811867819bf09b16d4617067da9d48928
Mert Can Altın <37827216+mertcanaltin@users.noreply.github.com>
2018-11-23 03:30:25 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,21 @@
+onst Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`bot sunuculara giriş yaptı Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'sa') {
+    msg.reply('Aleykümselam!');
+  }
+});
+
+client.on('message',msg =>{
+  if (msg.content == 'mert') {
+    msg.reply('Twitch de  yayında')
+  }
+})
+
+client.login('NTE1MzE2MDQ0Mzk5OTAyNzYy.DtjVfQ.H1Vj4ZW7OGkWUkcAz1ew30Qmz0o
+');
