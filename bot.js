40c09f5ba2a990c76a41cadfbbd5916eab3530d6
Mert Can Altın <37827216+mertcanaltin@users.noreply.github.com>
2018-11-29 01:22:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,11 +11,28 @@ client.on('message', msg => {
   }
 });
 
+
+client.om('message',msg =>{
+  if(msg.content== 'merhaba nasılsın'){
+    msg.reply('çok saol ben çok iyiyim ya sen ?')
+  } 
+})
+
 client.on('message',msg =>{
   if (msg.content == 'mert') {
     msg.reply('Twitch de  yayında')
   }
 })
+
+ if(command === "küfür") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  }
 
 client.login('token!!!!
 ');
